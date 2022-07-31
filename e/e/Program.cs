c7dce5212675a3353565419c5b144194d9f27b39
Rodolpho Vianna Santoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2022-07-31 15:41:03 -0300

    Add files via upload

---
+++ e/e/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1000;
+            int b ;
+            for (a = 1000; a < 1100; a += 10)
+            {
+                b = a << 5;
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
